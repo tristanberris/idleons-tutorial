e803df01d422497cfd3273694d949438ed4a07c9
tristanberris <coding.berris.tristan@gmail.com>
2025-03-29 00:52:29 -0400

    1st

--- data_structures/items/crystal.gd
+++ data_structures/items/crystal.gd
@@ -5,10 +5,28 @@ extends Resource
 ##Idealon value
 var _value : int 
 
+var _name:String
+
 ## Constructor
 ### Method that is called when a class is being created in an object.
 ### When the item is created, the value will be set within the constructor
-func _init() -> void:
-	_value = randi_range(5, 25)
+#func _init() -> void:
+	#_value = randi_range(5, 25)
 	
+static func create_common_crystal()->CrystalResource:
+	var new_crystal:CrystalResource = CrystalResource.new()
+	
+	new_crystal._name = "Common Crystal"
+	new_crystal._value = randi_range(5,10)
+	
+	
+	return new_crystal
+	
+static func create_rare_crystal()->CrystalResource:
+	var new_crystal:CrystalResource = CrystalResource.new()
+	
+	new_crystal._name = "Rare Crystal"
+	new_crystal._value = randi_range(100,250)
+	
+	return new_crystal	
 	
--- scenes/singletons/excavator/excavator.gd
+++ scenes/singletons/excavator/excavator.gd
@@ -15,9 +15,29 @@ func _init()->void:
 ## Public entry point triggering an excavating action.
 ## This is set to match what was in the first scene soo far in the series. 
 func excavate()->void:
-	IdleonsManager.ref.create_idleons(5)
-	var roll:int = randi_range(0,99)
+	var crystal_dust_amount:int = randf_range(2,5)
 	
-	if roll<33:
-		var crystal:CrystalResource = CrystalResource.new()
-		CrystalInventory.ref.add_crystal(crystal)
+	CrystalDustManager.ref.create(crystal_dust_amount)
+	
+	## This is essentially a 10% chance. 
+	var crystal_creation_roll:int = randf_range(0,99)
+	if crystal_creation_roll <= 9:
+		_create_a_crystal()
+
+## Create a random amount of crystal dust	
+func _create_crystal_dust()->void:
+	var crystal_dust_amount:int=randi_range(2,5)
+	CrystalDustManager.ref.create(crystal_dust_amount)
+	
+## Create a random crustal and add it to the inventory
+func _create_a_crystal()->void:
+	var rare_crystal_roll:int=randi_range(0,99)
+	
+	var crystal:CrystalResource
+	
+	if rare_crystal_roll<=9:
+		crystal = CrystalResource.create_rare_crystal()
+	else:
+		crystal = CrystalResource.create_common_crystal()
+	
+	CrystalInventory.ref.add_crystal(crystal)
